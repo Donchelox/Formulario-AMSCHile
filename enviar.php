946dbf4be776626fdbd006ce7497364ce1299c30
Donchelox <inside.4.me@gmail.com>
2014-08-14 10:17:04 -0400

    v final

--- enviar.php
+++ enviar.php
@@ -5,25 +5,50 @@
 	$name = $_POST['sender_name'];
 	$mail_from = $_POST['sender_email'];
 	$phone = $_POST['sender_phone'];
-	$phone = $_POST['company'];
-	$host_period = $_POST['country'];
+	$company = $_POST['company'];
+	$country = $_POST['country'];
+
+	$servicio = $_POST['servicio'];
+	$transporte = $_POST['transporte'];
+	$origen = $_POST['origen'];
+	$destino = $_POST['destino'];
+	$start_date = $_POST['start_date'];
+	$end_date = $_POST['end_date'];
+
+	$quantity = $_POST['quantity'];
+	$total_weight = $_POST['total_weight'];
+	$dimension = $_POST['dimension'];
+	$declared_value = $_POST['declared_value'];
 	
 	// Construct subject of the email
 	$subject = 'Cotización desde la web AMSChile ' . $name;
 
 	// Construct email body
-	$body_message = 'From: ' . $name . "\r\n";
+
+	$body_message = 'Nombre: ' . $name . "\r\n";
 	$body_message .= 'E-mail: ' . $mail_from . "\r\n";
-	$body_message .= 'Phone: ' . $phone . "\r\n";
-	$body_message .= 'country: ' . $message;
-	
-	$body_message .= 'User selected ' . $host_plan . ' hosting plan' . "\r\n";
-	$body_message .= 'Additional options selected: ' . $additional_options . "\r\n";
-	$body_message .= 'Hosting period: ' . $host_period . "\r\n";
+	$body_message .= 'Teléfono: ' . $phone . "\r\n";		
+	$body_message .= 'Compañia: ' . $company . "\r\n";
+	$body_message .= 'País: ' . $country . "\r\n" . "\r\n";
+
+	$body_message .= 'Servicio: ' . $servicio . "\r\n";
+	$body_message .= 'Transporte: ' . $transporte . "\r\n";
+	$body_message .= 'Origen: ' . $origen . "\r\n";
+	$body_message .= 'Destino: ' . $destino . "\r\n";
+	$body_message .= 'Fecha de inicio del servicio: ' . $start_date . "\r\n";
+	$body_message .= 'Fecha de termino del servicio: ' . $end_date . "\r\n" . "\r\n";
+
+	$body_message .= 'Numero de piezas: ' . $quantity . "\r\n";
+	$body_message .= 'Peso total: ' . $total_weight . "\r\n";
+	$body_message .= 'Dimensión: ' . $dimension . "\r\n";
+	$body_message .= 'Valor declarado (US$): ' . $declared_value . "\r\n";
+
+
 
 	// Construct headers of the message
 	$headers = 'From: ' . $mail_from . "\r\n";
 	$headers .= 'Reply-To: ' . $mail_from . "\r\n";
+
 
 	$mail_sent = mail($mail_to, $subject, $body_message, $headers);
 
